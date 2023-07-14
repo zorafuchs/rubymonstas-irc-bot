1b176f4f46074dae32d27fff6cbc2850802a35dd
Zora Fuchs <13620981+zorafuchs@users.noreply.github.com>
2023-07-14 16:30:16 +0000

    Make bot handling stateless messages

--- message-handler.rb
+++ message-handler.rb
@@ -1,18 +1,19 @@
 require_relative './irc-connector.rb'
+require_relative './stateless-reaction-controller.rb'
 
 class MessageHandler
   def initialize
+    @stateless_reaction_controller = StatelessReactionController.new
     @irc_connector = IRCConnector.new
     @irc_connector.connect
     @irc_connector.listen_for_messages(self.method(:receive))
   end
 
   def receive(message)
-    handle_channel_message(message)
+    handle_stateless(message)
   end
 
-  def handle_channel_message(message)
-    @irc_connector.send("PRIVMSG #rubymonstas :blubb")
+  def handle_stateless(message)
+    @irc_connector.send("PRIVMSG #rubymonstas :" + @stateless_reaction_controller.stateless_reaction_to(message)) if @stateless_reaction_controller.stateless_reaction_to(message) 
   end
 end
-
